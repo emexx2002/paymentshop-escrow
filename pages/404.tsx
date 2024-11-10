c8cb8806d61203ea9a2778509a2ee5d36c685203
emexx2002 <aguchukwuemekag@gmail.com>
2024-11-10 09:01:54 +0100

    redirecting

---
+++ pages/404.tsx
@@ -0,0 +1,28 @@
+// pages/404.js
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+import { useAuth } from '../zustand/auth.store';
+
+export default function Custom404() {
+  const router = useRouter();
+
+  useEffect(() => {
+    // Check for the SESSION cookie
+        const sessionToken =  useAuth.getState().token;
+
+    // Redirect based on whether the token is present
+    if (sessionToken) {
+      // If token is present, redirect to dashboard
+      router.replace('/dashboard');
+    } else {
+      // If token is absent, redirect to login
+      router.replace('/login');
+    }
+  }, [router]);
+
+  return (
+    <div>
+      <p>Redirecting...</p>
+    </div>
+  );
+}
